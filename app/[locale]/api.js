d2c9ccad6c94bd9326c593315139e3b6357224e9
ibrahem hassan <hibrahem266@gmail.com>
2023-09-27 18:50:57 +0300

    edit url

--- app/[locale]/api.js
+++ app/[locale]/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://education.jabal-p.com/',
+  baseURL: 'https://education.aquadic.com/',
   // Additional configurations, headers, etc.
 });
 
@@ -9,4 +9,4 @@ export default api;
 
 
 
-export const BASE_URL = 'https://education.jabal-p.com';
+export const BASE_URL = 'https://education.aquadic.com';
